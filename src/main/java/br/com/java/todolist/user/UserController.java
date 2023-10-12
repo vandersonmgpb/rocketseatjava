02027cabfb497f8f71536a70a7d86a3cf1151fd4
Vanderson1022 <vanderson1022@gmail.com>
2023-10-12 10:24:14 -0300

    Segunda aula rockeseat java Spring Boot

--- src/main/java/br/com/java/todolist/user/UserController.java
+++ src/main/java/br/com/java/todolist/user/UserController.java
@@ -1,5 +1,8 @@
 package br.com.java.todolist.user;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,7 +16,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/users")
 public class UserController {
-    
+
     /**
      * String (texto)
      * Integer (int) numeros inteiros
@@ -21,18 +24,28 @@ public class UserController {
      * Float (float) números 0.0000
      * Char (A C)
      * Date (data)
-     * Void - não retorna valor/resultado 
+     * Void - não retorna valor/resultado
      */
+    @Autowired
+    private IUserRepository userRepository;
 
-     /**
-      * Body
-      * @param userModel
-      */
-    @PostMapping("/")  
-    public void create (@RequestBody UserModel userModel){
+    /**
+     * Body
+     * 
+     * @param userModel
+     */
+    @PostMapping("/")
+    public ResponseEntity create(@RequestBody UserModel userModel) {
+        var user = this.userRepository.findByName(userModel.getUsername());
 
-        System.out.println("userModel.name");
+        if(user != null){
+            //Mensagem de erro
+            //Status code
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Usuário já existe");
 
+        var userCreated = this.userRepository.save(userModel);
+        return ResponseEntity.status(HttpStatus.CREATED).body(userCreated);
+        }
     }
 
 }
